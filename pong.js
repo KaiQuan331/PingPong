2058f6db26dc7340777eaa3b90c94850d9430983
Kai Quander <kq1003@gmail.com>
2020-06-25 11:05:17 -0400

    Added new player

--- pong.js
+++ pong.js
@@ -11,12 +11,36 @@ color = get_random_color()
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth) / 2;
+var paddleY = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+var score = 0
 
-
-
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 
 //Functions
+
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
 
 function get_random_color() {
     var letters = 'ABCDE'.split('');
@@ -38,7 +62,14 @@ function drawBall() {
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "blue";
+    
+
+
+    ctx.fillRect(paddleY, canvas.height-600, paddleWidth, paddleHeight)
+    ctx.fillStyle = 'green'
+
+    
     ctx.fill();
     ctx.closePath();
 }
@@ -46,14 +77,46 @@ function drawPaddle() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
+    drawScore();
+
+function drawScore() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText("Score: "+score, 8, 20);
+    }
 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
         color = get_random_color();
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
         color = get_random_color();
+    }
+
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
     }
 
     x += dx;
